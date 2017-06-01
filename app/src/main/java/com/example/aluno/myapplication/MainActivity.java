15d8621e9bd8f8e5e13859b4c9abba371684ddab
Rodrigo <rodrigo19962010@live.com>
2017-06-01 11:54:15 -0300

    Insert more datas

--- app/src/main/java/com/example/aluno/myapplication/MainActivity.java
+++ app/src/main/java/com/example/aluno/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aluno.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -16,12 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                // VanVan, it is here that data stays.
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+                new DataPoint(2, 8),
+                new DataPoint(3, 4),
+                new DataPoint(3, 4),
+                new DataPoint(4, 4),
+                new DataPoint(5, 7),
+                new DataPoint(7, 4),
+                new DataPoint(8, 3),
+                new DataPoint(9, 8)
         });
+        graph.addSeries(series);
     }
 }
